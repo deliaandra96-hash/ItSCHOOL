00f246fa66a58c232c733d7396f53d05008354d1
Delia <delia.andra@gmail.com>
2025-11-22 19:00:13 +0200

    first session

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,52 @@
-import java.util.Arrays;
+public class Main {
 
-import static java.lang.System.*;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-  public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        out.println("HELLO TO JAVA");
+           //Afisan un mesaj simplu la inceputul programului
+        System.out.println("Hello to Java ");
 
-       int age = 29 ;
-       short age2 = 30 ;
-       long number = 222000000L ;
-        double weight = 53.0d ;
-        float weightFloat =53.5F ;
-        boolean alive = true ;
-        char litera = 'A' ;
-        byte something = 0 ;
-      out.println(" voi printa varsta mea : " +age);  ;
-      out.println(" voi printa age 2 , nu stiu ce e asta " +age2) ;
-      out.println("voi printa acuma number : " + number);
-        for (String s : Arrays.asList(" cantaresc " + weight, " cantaresc mai exact " + weightFloat, " sunt in viata ? " + alive, " Prima litera din alfabet este " + litera)) {
-            out.println(s) ;
-        }
+         // Declararea si initializarea variabilelor
 
-        String name = " Delia " ;
-        out.println("Ma numest " +name) ;
+          int age = 29 ;
+          short age2 =30;
+          long number = 2222200;
+          double weight = 53.0d;
+          float weightFloat = 115.5f ;
+          boolean alive = true ;
+          char litera = 'A' ;
+          byte something = 0 ;
 
-        int numar1 = 10 ;
-        int numar2 = 5 ;
-        int sum ;
-        sum = numar1 + numar2 ;
-        out.println("adunare:" +sum);
-        out.println(numar1 + numar2);
-        int dif = numar1 - numar2;
-        out.println("scadere:" +dif);
-        int mul = numar1 * numar2;
-        out.println("inmultire:" +mul);
-        int sub = numar1 / numar2;
-        out.println("impartire: " +sub) ;
-    }
+          //Afisarea variabilelor
+
+        System.out.println("Varsta mea este: " +age);
+        System.out.println("Alta varsta (short):" +number);
+        System.out.println(" Un numar foarte mare (long)" +number);
+        System.out.println("Greutate (double)" +weight);
+        System.out.println("Greutate mai exacta (float)" +weightFloat);
+        System.out.println("Sunt in viata ? " +alive);
+        System.out.println("Prima litera din alfabet" +litera);
+        System.out.println("Valoare byte " +something) ;
+
+        //Variabila de tip String
+
+        String name = "Delia " ;
+        System.out.println("Ma numesc :" +name) ;
+
+        //Operatii aritmetice de baza
+
+        int numar1 =10;
+        int numar2 =5;
+
+        int sum =numar1 + numar2;
+        int dif =numar1 - numar2 ;
+        int mul= numar1 * numar2;
+        int div = numar1 / numar2 ;
+
+        System.out.println("Adunare:" +sum);
+        System.out.println("Adunare directa :" +(numar1 + numar2));
+        System.out.println("Scadere:" +dif);
+        System.out.println("Inmultire:" +mul);
+        System.out.println("Impartire:" +div);
+
+
+      }
 }
